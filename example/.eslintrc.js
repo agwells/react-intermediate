fc25c45a75b379e5e2522a04e8629bff8248f914
Aaron Wells <aaronw@catalyst.net.nz>
2021-05-14 17:53:43 +1200

    Switch to friendlier ESLint config
    This is based on the ESLint config used by Create React App. It is
    much less opinionated than Airbnb, so it mostly provides warnings
    about things that are likely bugs, and rarely about code style.
    
    I've also disabled the jsx-a11y plugin, because it also creates a lot
    of warnings when you're trying to quickly hack together a program
    in a half-day React course.

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,8 +1,4 @@
-/**
- * current style guide extends 3 other style guides
- * airbnb, typescript, prettier
- * all conflicting rules should be switched off in preceeding guide
- */
+const restrictedGlobals = require('confusing-browser-globals');
 
 module.exports = {
   env: {
@@ -16,16 +12,19 @@ module.exports = {
         paths: ['src'],
       },
     },
+    react: {
+      version: 'detect',
+    },
   },
   extends: [
-    'airbnb', // javascript syntax
-    'airbnb/hooks', // rules for react hooks
-    'plugin:@typescript-eslint/recommended', // typescript syntax
+    'eslint:recommended', // possible bugs ("code smells")
+    'plugin:react/recommended',
     'prettier', // code formating rules
     'prettier/react',
     'prettier/@typescript-eslint',
   ],
-  plugins: ['react', 'import', 'prettier', '@typescript-eslint'],
+  plugins: ['react', 'import', '@typescript-eslint', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     ecmaFeatures: {
@@ -39,45 +38,193 @@ module.exports = {
     process: true,
   },
   rules: {
-    'comma-dangle': [
+    // Based on eslint-config-react-app, because its rules are very unopinionated,
+    // good for getting things done quickly during training.
+    //
+    // In actual development we use the airbnb eslint rules.
+
+    // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+    'default-case': 'off',
+    // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+    'no-dupe-class-members': 'off',
+    // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+    'no-undef': 'off',
+
+    // Add TypeScript specific rules (and turn off ESLint equivalents)
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': 'warn',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'warn',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
       'warn',
       {
-        arrays: 'only-multiline',
-        objects: 'only-multiline',
-        imports: 'only-multiline',
-        exports: 'only-multiline',
-        functions: 'ignore',
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false,
       },
     ],
-    'import/no-named-as-default': 0,
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/extensions': 0,
-    'max-len': ['warn', { code: 120, ignoreComments: true }],
-    quotes: ['warn', 'single', { allowTemplateLiterals: true }],
-    'import/no-unresolved': 0,
-    'prettier/prettier': ['error'],
-    'react/destructuring-assignment': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'react/jsx-wrap-multilines': 0,
-    'react/no-unused-state': 0,
-    'react/prop-types': 0,
-    'react/require-default-props': 0,
-    'no-use-before-define': 'off',
-    'no-plusplus': 0,
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-object-literal-type-assertion': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'react/jsx-no-bind': [
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': [
       'error',
       {
-        ignoreDOMComponents: false,
-        ignoreRefs: false,
-        allowArrowFunctions: true,
-        allowFunctions: false,
-        allowBind: false,
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
       },
     ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'warn',
+
+    // The ESLint browser environment defines all browser globals as valid,
+    // even though most people don't know some of them exist (e.g. `name` or `status`).
+    // This is dangerous as it hides accidentally undefined variables.
+    // We blacklist the globals that we deem potentially confusing.
+    // To use them, explicitly reference them, e.g. `window.name` or `window.status`.
+    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+
+    // http://eslint.org/docs/rules/
+    'array-callback-return': 'warn',
+    'dot-location': ['warn', 'property'],
+    eqeqeq: ['warn', 'smart'],
+    'new-parens': 'warn',
+    'no-caller': 'warn',
+    'no-cond-assign': ['warn', 'except-parens'],
+    'no-const-assign': 'warn',
+    'no-control-regex': 'warn',
+    'no-delete-var': 'warn',
+    'no-dupe-args': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-duplicate-case': 'warn',
+    'no-empty-character-class': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-eval': 'warn',
+    'no-ex-assign': 'warn',
+    'no-extend-native': 'warn',
+    'no-extra-bind': 'warn',
+    'no-extra-label': 'warn',
+    'no-fallthrough': 'warn',
+    'no-func-assign': 'warn',
+    'no-implied-eval': 'warn',
+    'no-invalid-regexp': 'warn',
+    'no-iterator': 'warn',
+    'no-label-var': 'warn',
+    'no-labels': ['warn', { allowLoop: true, allowSwitch: false }],
+    'no-lone-blocks': 'warn',
+    'no-loop-func': 'warn',
+    'no-mixed-operators': [
+      'warn',
+      {
+        groups: [
+          ['&', '|', '^', '~', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+          ['&&', '||'],
+          ['in', 'instanceof'],
+        ],
+        allowSamePrecedence: false,
+      },
+    ],
+    'no-multi-str': 'warn',
+    'no-native-reassign': 'warn',
+    'no-negated-in-lhs': 'warn',
+    'no-new-func': 'warn',
+    'no-new-object': 'warn',
+    'no-new-symbol': 'warn',
+    'no-new-wrappers': 'warn',
+    'no-obj-calls': 'warn',
+    'no-octal': 'warn',
+    'no-octal-escape': 'warn',
+    'no-regex-spaces': 'warn',
+    'no-restricted-syntax': ['warn', 'WithStatement'],
+    'no-script-url': 'warn',
+    'no-self-assign': 'warn',
+    'no-self-compare': 'warn',
+    'no-sequences': 'warn',
+    'no-shadow-restricted-names': 'warn',
+    'no-sparse-arrays': 'warn',
+    'no-template-curly-in-string': 'warn',
+    'no-this-before-super': 'warn',
+    'no-throw-literal': 'warn',
+    'no-unreachable': 'warn',
+    'no-unused-labels': 'warn',
+    'no-useless-computed-key': 'warn',
+    'no-useless-concat': 'warn',
+    'no-useless-escape': 'warn',
+    'no-useless-rename': [
+      'warn',
+      {
+        ignoreDestructuring: false,
+        ignoreImport: false,
+        ignoreExport: false,
+      },
+    ],
+    'no-with': 'warn',
+    'no-whitespace-before-property': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'require-yield': 'warn',
+    'rest-spread-spacing': ['warn', 'never'],
+    strict: ['warn', 'never'],
+    'unicode-bom': ['warn', 'never'],
+    'use-isnan': 'warn',
+    'valid-typeof': 'warn',
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'require',
+        property: 'ensure',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+      {
+        object: 'System',
+        property: 'import',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+    ],
+    'getter-return': 'warn',
+
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
+    'import/first': 'error',
+    'import/no-amd': 'error',
+    'import/no-anonymous-default-export': 'warn',
+    'import/no-webpack-loader-syntax': 'error',
+
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': 'warn',
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
+      {
+        allowAllCaps: true,
+        ignore: [],
+      },
+    ],
+    'react/no-danger-with-children': 'warn',
+    // Disabled because of undesirable warnings
+    // See https://github.com/facebook/create-react-app/issues/5204 for
+    // blockers until its re-enabled
+    // 'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+
+    // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
+    'react-hooks/rules-of-hooks': 'error',
   },
 };
